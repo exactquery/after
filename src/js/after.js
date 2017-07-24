3ebc1f6558e880837967d00f78187af9dab4faab
AMJones <aaronj@jonesiscoding.com>
2017-07-24 08:13:50 -0500

    Initial commit.

---
+++ src/js/after.js
@@ -0,0 +1,36 @@
+
+/**
+ * Fires an event after the last repeated event finishes.  Useful for resize & similar events.
+ *
+ * @author  AMJones <am@jonesiscoding.com>
+ * @returns {jQuery}
+ */
+;(function($){
+  $.fn.extend({
+    after: function( eventName, callback, options ) {
+
+      var plugin = this;
+      var $plugin = $(plugin);
+      var eTimer;
+
+      plugin.defaultOptions = {
+        'interval': 250,
+        'preventDefault': false,
+        'stopPropagation': false,
+        'stopImmediatePropagation': false
+      };
+
+      var settings = $.extend({}, plugin.defaultOptions, options);
+
+      return $plugin.on(eventName, function(e) {
+
+        if ( settings.preventDefault ) { e.preventDefault(); }
+        if ( settings.stopPropagation ) { e.stopPropagation(); }
+        if ( settings.stopImmediatePropagation ) { e.stopImmediatePropagation(); }
+
+        clearTimeout(eTimer);
+        eTimer = setTimeout(callback, settings.interval);
+      });
+    }
+  });
+})(jQuery);
